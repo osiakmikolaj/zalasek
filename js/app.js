68f093eb28d8528a07e5ca67aa042da4ae1bf466
Mikołaj Osiak <osiakmikolaj@yahoo.com>
2023-03-21 22:10:44 +0100

    sw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
         .register("/js/sw.js")
-        .then((reg) => console.log(reg))
+        .then()
         .catch((err) => console.log(err));
 }
