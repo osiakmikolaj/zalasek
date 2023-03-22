0978f11fb2ba092655ac1b8b4bbe9ba95ffb5352
Mikołaj Osiak <osiakmikolaj@yahoo.com>
2023-03-22 18:39:25 +0100

    new style

--- js/header.js
+++ js/header.js
@@ -1,12 +1,12 @@
 const header = document.getElementById("header");
-// porzedni scroll jest równy akutalnemu położeniu strony
+
 let prevScroll = window.scrollY;
 let road = window.scrollY;
-// na event scrolowania
+
 window.onscroll = () => {
     let curScroll = window.scrollY;
     road = curScroll;
     if (prevScroll > curScroll) header.style.top = "0px";
-    if (road > 100) if (prevScroll < curScroll) header.style.top = "-130px";
+    if (road > 100) if (prevScroll < curScroll) header.style.top = "-100px";
     prevScroll = curScroll;
 };
