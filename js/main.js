b61f207cbf1a179dc8bf711fb1e7e5f17e70b82f
Mikołaj Osiak <osiakmikolaj@gmail.com>
2024-05-14 23:17:31 +0200

    New website made with bootstrap

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const carouselWide = new bootstrap.Carousel("#carouselWide");
+const carouselMobile = new bootstrap.Carousel("#carouselMobile");
+const carouselSponsors = new bootstrap.Carousel("#carouselSponsors");
+const carosuelAccommodation = new bootstrap.Carousel("#carouselAccommodation");
+const carosuelAccommodationMobile = new bootstrap.Carousel("#carosuelAccommodationMobile");
